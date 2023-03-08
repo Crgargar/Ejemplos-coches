25a5f218d90b803dc9d2265b874049b1ac7098e0
García Garrido, Cristian <94846219+Crgargar@users.noreply.github.com>
2023-03-08 12:26:27 +0100

    terminado

--- src/app/pagina2/pagina2.page.ts
+++ src/app/pagina2/pagina2.page.ts
@@ -23,12 +23,17 @@ export class Pagina2Page implements OnInit {
 }
 
 loadMap() {
-  let latitud = 36.6797047;
-  let longitud = -5.4470656;
+  let marker;
+  let latitud = 36.70915575169545;
+  let longitud = -6.119396216905574;
   let zoom = 17;
   this.map = L.map("mapId").setView([latitud, longitud], zoom);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
       .addTo(this.map);
+  marker = L.marker([latitud, longitud]).addTo(this.map);
+  L.marker([36.70915575169545, -6.119396216905574]).addTo(this.map).bindPopup('Automoción Terry').openPopup();
+
+      
 }
 
 }
